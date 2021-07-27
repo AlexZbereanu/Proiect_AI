dc73adc9b5722163162b70555b2a6dcf72d14604
AlexZbereanu <57541189+AlexZbereanu@users.noreply.github.com>
2021-07-27 11:28:10 +0300

    ..

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
 const bodyParser = require('body-parser')
+const brain = require('brain.js')
 
 const app = express();
 
@@ -21,34 +22,55 @@ app.use(bodyParser.urlencoded({ extended: true }));
  	citire intrebari din fisierul json
 */
 const fs = require('fs');
-const { render } = require('ejs');
 let rawdata = fs.readFileSync('intrebari.json');
 let intrebare = JSON.parse(rawdata);
 let listaIntrebari = [];
 for(var i=0; i < intrebare.intrebari.length;i++)
 	listaIntrebari.push(intrebare.intrebari[i])
 
+// 1=> sanatos    0.5=> racit,    0 => covid
+var data = [{ input: {a: 1.0, b: 1.0}, output:{h: 1.0}},
+    { input: {a: 1.0, b: 0.0}, output:{h: 0.5}},
+    { input: {a: 0.0, b: 0.0}, output:{h: 0.0} }
+]
+
+var net = new brain.NeuralNetwork();
+
 // la accesarea din browser adresei http://localhost:6789/ se va returna textul 'Hello World'
 // proprietățile obiectului Request - req - https://expressjs.com/en/api.html#req
 // proprietățile obiectului Response - res - https://expressjs.com/en/api.html#res
+app.get('/', (req,res) => {
+    res.render('index');
+});
 
-app.get('/', (req, res) => {
+app.get('/chestionar', (req, res) => {
 	res.render('chestionar', {intrebari: listaIntrebari});
 });
 
+
 app.post('/rezultat-chestionar', (req, res) => {
+    
 	console.log(req.body);
-    var result = "Varsta: " + JSON.stringify(req.body.type0) +
-     "<br> Gust: " + JSON.stringify(req.body.type1) +
-      "<br> Frisoane: " + JSON.stringify(req.body.type2) +
-       "<br> Miros: " + JSON.stringify(req.body.type3) +
-       "<br> Probleme respiratorii: " + JSON.stringify(req.body.type4) +
-       "<br> Durere de cap: " + JSON.stringify(req.body.type5) 
-	res.send(result);
+    
+    var result ; //"Varsta: " + JSON.stringify(req.body.type0) +
+     //"<br> Gust: " + JSON.stringify(req.body.type1) +
+      //"<br> Frisoane: " + JSON.stringify(req.body.type2) +
+       //"<br> Miros: " + JSON.stringify(req.body.type3) +
+       //"<br> Probleme respiratorii: " + JSON.stringify(req.body.type4) +
+       //"<br> Durere de cap: " + JSON.stringify(req.body.type5) ;
+
+    const getData = async() => {
+        net.train(data, {log: true});
+    }
+
+    getData().then(result =  net.run({a:1.0, b:0.0}).h);
+    
+    res.render('harta', {result: result});
+	
 });
 
 app.get('/harta', (req, res) =>{
     res.render('harta');
 });
 
-app.listen(port, () => console.log(`Serverul rulează la adresa http://localhost:`));
+app.listen(port, () => console.log(`Serverul rulează la adresa http://localhost:${port}`));
